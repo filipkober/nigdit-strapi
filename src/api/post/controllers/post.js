1f6fbd1ecb295cd726c9f69b1b4afa8b672d3fac
Filip Kober <koberfilip2@gmail.com>
2023-03-29 19:43:25 +0200

    added post upvote downvote

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -162,5 +162,69 @@ module.exports = createCoreController('api::post.post', ({strapi})=>{
                 ctx.body = err
             }
         },
+        async upVote(ctx){
+            const user = ctx.state.user
+            const id = ctx.params.id
+            const clonedVotes = JSON.parse(JSON.stringify(user.votes))
+            const post = await strapi.entityService.findOne("api::post.post", id)
+            if(!post) return ctx.send("Post not found", 404)
+            let postVotes = post.Votes
+            if(clonedVotes.upvotes.posts.includes(id)){
+                clonedVotes.upvotes.posts = clonedVotes.upvotes.posts.filter((postId)=>postId!=id)
+                postVotes--
+            } else if (clonedVotes.downvotes.posts.includes(id)){
+                clonedVotes.downvotes.posts = clonedVotes.downvotes.posts.filter((postId)=>postId!=id)
+                clonedVotes.upvotes.posts.push(id)
+                postVotes+=2
+            } else {
+                clonedVotes.upvotes.posts.push(id)
+                postVotes++
+            }
+
+            await strapi.entityService.update("api::post.post", id, {
+                data:{
+                    Votes: postVotes
+                }
+            })
+
+            const updatedUser = await strapi.entityService.update("plugin::users-permissions.user", user.id, {
+                data:{
+                    votes: clonedVotes
+                }
+            })
+            ctx.send(updatedUser.votes, 200)
+        },
+        async downVote(ctx){
+            const user = ctx.state.user
+            const id = ctx.params.id
+            const clonedVotes = JSON.parse(JSON.stringify(user.votes))
+            const post = await strapi.entityService.findOne("api::post.post", id)
+            if(!post) return ctx.send("Post not found", 404)
+            let postVotes = post.Votes
+            if(clonedVotes.downvotes.posts.includes(id)){
+                clonedVotes.downvotes.posts = clonedVotes.downvotes.posts.filter((postId)=>postId!=id)
+                postVotes++
+            } else if (clonedVotes.upvotes.posts.includes(id)){
+                clonedVotes.upvotes.posts = clonedVotes.upvotes.posts.filter((postId)=>postId!=id)
+                clonedVotes.downvotes.posts.push(id)
+                postVotes-=2
+            } else {
+                clonedVotes.downvotes.posts.push(id)
+                postVotes--
+            }
+
+            await strapi.entityService.update("api::post.post", id, {
+                data:{
+                    Votes: postVotes
+                }
+            })
+
+            const updatedUser = await strapi.entityService.update("plugin::users-permissions.user", user.id, {
+                data:{
+                    votes: clonedVotes
+                }
+            })
+            ctx.send(updatedUser.votes, 200)
+        },
     }
 });
