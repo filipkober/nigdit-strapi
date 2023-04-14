72e9221c3c70d130a998c33ff45a31c4ecae7872
Damian Jedrzejewski <Damian035jedrzejewski@gmail.com>
2023-04-14 21:02:18 +0200

    dodano endpoint dołączania/wychodzenia z subnigdita: http://localhost:1338/api/subnigdits/join/[id]

---
+++ src/api/subnigdit/routes/join.js
@@ -0,0 +1,9 @@
+module.exports = {
+    routes: [
+        {
+          method: "POST",
+          path: "/subnigdits/join/:id",
+          handler: "subnigdit.joinSubnigdit",
+        },
+      ]
+}
