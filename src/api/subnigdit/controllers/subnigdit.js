a1d01ed12be7b26b85920b1b9c06313606d2a63b
Damian Jedrzejewski <Damian035jedrzejewski@gmail.com>
2023-04-14 21:02:18 +0200

    dodano endpoint dołączania/wychodzenia z subnigdita: http://localhost:1338/api/subnigdits/join/[id]

--- src/api/subnigdit/controllers/subnigdit.js
+++ src/api/subnigdit/controllers/subnigdit.js
@@ -6,4 +6,34 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::subnigdit.subnigdit');
+module.exports = createCoreController('api::subnigdit.subnigdit', ({strapi})=>{
+    return {
+        async joinSubnigdit(ctx)
+        {
+            const user = ctx.state.user
+            const idSubnigdita = ctx.params.id
+            const subnigdit = await strapi.entityService.findOne("api::subnigdit.subnigdit", idSubnigdita,{populate: "*"});
+            if(!subnigdit) 
+            {
+                return ctx.send("Subnigdit id not found", 404)
+            }
+            var subs = subnigdit.subscribers;
+            const ids = subs.map((i)=>{
+                return(i.id)
+                })
+            if(ids.includes(user.id))
+            {
+                subs = subs.filter((x)=>x.id!=user.id)
+            }
+            else
+            {        
+                subs.push(user)
+            }             
+            const updatedSubnigdit = await strapi.entityService.update("api::subnigdit.subnigdit", idSubnigdita, {
+                data: {
+                    subscribers: subs
+                }
+            }) 
+            ctx.send(subs, 200)
+        },
+    }});
