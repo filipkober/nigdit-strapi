588fe4b65e96a1c90c43eac8a64ca31abe3eb6a9
Damian Jedrzejewski <Damian035jedrzejewski@gmail.com>
2023-05-23 20:21:46 +0200

    poprawki do feeda

--- src/api/post/routes/feed.js
+++ src/api/post/routes/feed.js
@@ -1,45 +1,54 @@
 module.exports = {
     routes: [
         {
-            method: 'POST',          //GET | POST | DELETE | PUT
+            method: 'GET',          //GET | POST | DELETE | PUT
             path: '/posts/pop',           //http://localhost:1338/api/popular
             handler: 'post.getPop', //kontroler.metoda
-            // config: {
-            //     auth: false
-            // }
+            config: {
+                auth: false
+            }
         },
         {
-            method: 'POST',
+            method: 'GET',
             path: '/posts/top',
             handler: 'post.getTop',
+            config: {
+                auth: false
+            }
         },
         {
-            method: 'POST',
+            method: 'GET',
             path: '/posts/new',
             handler: 'post.getNew',
+            config: {
+                auth: false
+            }
         },
         {
-            method: 'POST',
+            method: 'GET',
             path: '/posts/hot',
             handler: 'post.getHot',
+            config: {
+                auth: false
+            }
         },
         {
-            method: 'POST',
+            method: 'GET',
             path: '/posts/popSub',
             handler: 'post.getPopSub',
         },
         {
-            method: 'POST',
+            method: 'GET',
             path: '/posts/topSub',
             handler: 'post.getTopSub',
         },
         {
-            method: 'POST',
+            method: 'GET',
             path: '/posts/newSub',
             handler: 'post.getNewSub',
         },
         {
-            method: 'POST',
+            method: 'GET',
             path: '/posts/hotSub',
             handler: 'post.getHotSub',
         }
